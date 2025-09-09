af8a601c42088862cd7a7b7b62c297a801bb077a
tanya <90786339+wyutan@users.noreply.github.com>
2025-09-09 19:42:57 +0800

    Update storage.ts

--- docs/.vuepress/notes/storage.ts
+++ docs/.vuepress/notes/storage.ts
@@ -4,6 +4,6 @@ export default defineNoteConfig({
   dir: '/storage/',
   link: '/storage/',
   sidebar: [
-    { text: '存储笔记', prefix: 'storage', collapsed: false, items: 'auto', },
+    { text: '存储笔记', prefix: 'note', collapsed: false, items: 'auto', },
   ],
 })
