8d496b45733e92596a8a15a762824c6512678a84
tanya <90786339+wyutan@users.noreply.github.com>
2026-02-15 23:10:47 +0800

    Update AboutMe.vue

--- docs/.vuepress/theme/components/AboutMe.vue
+++ docs/.vuepress/theme/components/AboutMe.vue
@@ -6,7 +6,7 @@
   <router-link to="/blog/" class="head-social-avatar">
     <div class="about-me-name">
       <p>𝐻𝑒𝓁𝓁𝑜, 𝓃𝒾𝒸𝑒 𝓉𝑜 𝓂𝑒𝑒𝓉 𝓎𝑜𝓊 !👋</p>
-      <p class="about-me-card-text-big"style="font-size: 2rem; font-weight: 800;">你可以叫我 谈呀 .</p>
+      <p class="about-me-card-text-big">你可以叫我 谈呀 .</p>
       <p>是一名努力往全栈方向发展的基础设施工程师。</p>
     </div>
   </router-link>
@@ -25,6 +25,13 @@
 
   > p {
     color: var(--vp-c-text-1);
+    margin: 1.2rem 0;
   }
+}
+
+.about-me-card-text-big {
+  font-size: 2.2rem;   /* 设置为约2倍大小 */
+  font-weight: 800;    /* 极粗体 */
+  line-height: 1.2;    /* 大字体建议调小行高，视觉更紧凑 */
 }
 </style>
