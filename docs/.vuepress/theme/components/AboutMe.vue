8db02b36da4fc998441614832985d9d34658fee3
tanya <90786339+wyutan@users.noreply.github.com>
2026-02-15 23:55:55 +0800

    Update AboutMe.vue

--- docs/.vuepress/theme/components/AboutMe.vue
+++ docs/.vuepress/theme/components/AboutMe.vue
@@ -6,7 +6,7 @@
   <router-link to="/blog/" class="head-social-avatar">
     <div class="about-me-name">
       <p>𝐻𝑒𝓁𝓁𝑜, 𝓃𝒾𝒸𝑒 𝓉𝑜 𝓂𝑒𝑒𝓉 𝓎𝑜𝓊 !👋</p>
-      <p class="about-me-card-text-big">你可以叫我 谈呀 .</p>
+      <p class="about-me-card-text-big">你可以叫我 𝒯𝒶𝓃𝓎𝒶 .</p>
       <p>是一名努力往全栈方向发展的基础设施工程师。</p>
     </div>
   </router-link>
