ee9834f2b4b784ee2491f1d030afc20733254ef2
tanya <90786339+wyutan@users.noreply.github.com>
2026-02-15 21:00:40 +0800

    Update Custom.vue

--- docs/.vuepress/theme/components/Custom.vue
+++ docs/.vuepress/theme/components/Custom.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import AboutMeLife from './AboutMeLife.vue'
 
 const message = ref('Hello World!')
 </script>
@@ -7,5 +8,14 @@ const message = ref('Hello World!')
 <template>
   <div class="my-custom-content">
     {{ message }}
+<hr /> <AboutMeLife />
+    
   </div>
 </template>
+
+<style scoped>
+/* 3. 第三步：你可以给外层容器加点样式，防止组件撑破布局 */
+.my-custom-content {
+  padding: 20px;
+}
+</style>
