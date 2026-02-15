58aa57c05c23a53b70b3237e5f6141422da4d247
tanya <90786339+wyutan@users.noreply.github.com>
2026-02-15 22:23:33 +0800

    Update AboutMe.vue

--- docs/.vuepress/theme/components/AboutMe.vue
+++ docs/.vuepress/theme/components/AboutMe.vue
@@ -6,8 +6,8 @@
   <router-link to="/blog/" class="head-social-avatar">
     <div class="about-me-name">
       <p>𝐻𝑒𝓁𝓁𝑜, 𝓃𝒾𝒸𝑒 𝓉𝑜 𝓂𝑒𝑒𝓉 𝓎𝑜𝓊 !👋</p>
-      <p class="about-me-card-text-big">你可以叫我 𝓳𝓲𝓪𝓱𝓪𝓸 .</p>
-      <p>是一名全栈开发工程师。</p>
+      <p class="about-me-card-text-big">你可以叫我 谈呀 .</p>
+      <p>是一名追求全栈的基础设施运维工程师。</p>
     </div>
   </router-link>
 
