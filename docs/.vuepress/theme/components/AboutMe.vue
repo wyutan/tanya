5847b7700a010f1976fe099bec3267dc76bb5c61
tanya <90786339+wyutan@users.noreply.github.com>
2026-02-15 21:56:19 +0800

    Add files via upload

---
+++ docs/.vuepress/theme/components/AboutMe.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+</script>
+
+<template>
+  <router-link to="/blog/" class="head-social-avatar">
+    <div class="about-me-name">
+      <p>𝐻𝑒𝓁𝓁𝑜, 𝓃𝒾𝒸𝑒 𝓉𝑜 𝓂𝑒𝑒𝓉 𝓎𝑜𝓊 !👋</p>
+      <p class="about-me-card-text-big">你可以叫我 𝓳𝓲𝓪𝓱𝓪𝓸 .</p>
+      <p>是一名全栈开发工程师。</p>
+    </div>
+  </router-link>
+
+
+</template>
+
+<style scoped>
+.about-me-name {
+  border-radius: 12px;
+  justify-content: center;
+  padding: 2rem;
+  color: #fff;
+  background-image: linear-gradient(120deg, var(--vp-c-brand-3), var(--vp-c-brand-soft));
+  background-size: 200% 200%;
+
+  > p {
+    color: var(--vp-c-text-1);
+  }
+}
+</style>
