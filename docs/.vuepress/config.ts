6918ec6c1492d7be7fb1e277ecf49796456fb5c1
tanya <90786339+wyutan@users.noreply.github.com>
2026-02-16 13:36:52 +0000

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -139,9 +139,9 @@ export default defineUserConfig({
       fullPage: false,
 
       /** @see https://zhensherlock.github.io/watermark-js-plus/zh/config/ */
-      //watermarkOptions: {
-        //content: '谈呀',
-      //}
+      watermarkOptions: {
+        content: '谈呀',
+      }
     },
   }),
 })
