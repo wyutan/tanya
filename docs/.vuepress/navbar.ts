a5e8a9f58bbaf9292c193245bed5798c0ee65a1a
tanya <90786339+wyutan@users.noreply.github.com>
2026-02-16 13:56:45 +0800

    Update navbar.ts

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -5,17 +5,17 @@ export const navbar = defineNavbarConfig([
   //{ text: '博客', link: '/blog/' },
   //{ text: '标签', link: '/blog/tags/' },
   //{ text: '归档', link: '/blog/archives/' },
-  { text: '网络', icon: '', link: '/network/', },
+  { text: '网络', icon: '/icon/network.svg', link: '/network/', },
   {
     text: 'os',
-    icon: '',
+    icon: '/icon/os.svg',
     items: [
-      { text: 'linux', icon: '', link: '/os/linux/', },
-      { text: '云计算', icon: '', link: '/os/cloud/', },
+      { text: 'linux', icon: '/icon/linux.svg', link: '/os/linux/', },
+      { text: '云计算', icon: '/icon/cloud.svg', link: '/os/cloud/', },
     ]
   }, 
-  { text: '存储', icon: '', link: '/storage/', },
-  { text: '容器', icon: '', link: '/容器/', },
-  { text: '疑难杂症', icon: '',  link: '/疑难杂症/疑难杂症.md', },
-  { text: '关于', icon: '',  link: '/about/', },
+  { text: '存储', icon: '/icon/storage.svg', link: '/storage/', },
+  { text: '容器', icon: '/icon/Docker.svg', link: '/容器/', },
+  { text: '疑难杂症', icon: '/icon/question.svg',  link: '/疑难杂症/疑难杂症.md', },
+  { text: '关于', icon: '/icon/about.svg',  link: '/about/', },
 ])
