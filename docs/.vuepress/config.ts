6a7396f5cd70849dec8502e7165331d8393eae6b
tanya <90786339+wyutan@users.noreply.github.com>
2026-02-21 23:13:22 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -22,13 +22,10 @@ export default defineUserConfig({
     hostname: 'http://git.x-echo.net',
     search: { provider: 'local' },
 
-    //collections: [
-      //{
-        //type: 'post', // 替代原博客功能
-        //dir: '/about', // 指向 docs/blog 目录
-        //title: '' // 集合显示名称
-      //},
-    //],
+    collections: [
+      { type: 'post', dir: '/about', title: '' },
+      { type: 'doc', dir: '/os/linux', title: 'Linux学习笔记', sidebar: 'auto' }
+    ],
     comment: {
     provider: 'Giscus',
     comment: true,
