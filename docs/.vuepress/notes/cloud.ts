de714a11b7bdd886e4c90f7746f07ac929844367
tanya <90786339+wyutan@users.noreply.github.com>
2026-02-21 23:29:21 +0800

    Update cloud.ts

--- docs/.vuepress/notes/cloud.ts
+++ docs/.vuepress/notes/cloud.ts
@@ -3,8 +3,8 @@ import { defineNoteConfig } from 'vuepress-theme-plume'
 export default defineNoteConfig({
   dir: '/os/cloud',
   link: '/os/cloud',
-  sidebar: [
-    { text: '云计算学习笔记', link: '/os/cloud/', },
-    { text: '', prefix: 'note', collapsed: false, items: 'auto', },
-  ]
+  //sidebar: [
+    //{ text: '云计算学习笔记', link: '/os/cloud/', },
+    //{ text: '', prefix: 'note', collapsed: false, items: 'auto', },
+  //]
 })
