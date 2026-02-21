9ebd9e7eb8ac5a0c7e114de40864b7faac1a8bc7
tanya <90786339+wyutan@users.noreply.github.com>
2026-02-21 18:22:29 +0800

    Update AboutMe.vue

--- docs/.vuepress/theme/components/AboutMe.vue
+++ docs/.vuepress/theme/components/AboutMe.vue
@@ -8,7 +8,6 @@
       <p>𝐻𝑒𝓁𝓁𝑜, 𝓃𝒾𝒸𝑒 𝓉𝑜 𝓂𝑒𝑒𝓉 𝓎𝑜𝓊 !👋</p>
       <p class="about-me-card-text-big">你可以叫我 𝓽𝓪𝓷𝓽𝓪𝓷 .</p>
       <p>是一名努力奔向全栈的基础设施工程师。</p>
-      <div>
     </div></router-link>
   </template>
 
