3a833b695a0218bd0659b7b44dec6854d246e58f
tanya <90786339+wyutan@users.noreply.github.com>
2026-02-21 23:36:43 +0800

    Update 容器.ts

--- docs/.vuepress/notes/容器.ts
+++ docs/.vuepress/notes/容器.ts
@@ -3,12 +3,4 @@ import { defineNoteConfig } from 'vuepress-theme-plume'
 export default defineNoteConfig({
   dir: '/容器/',
   link: '/容器/',
-  /*
-  sidebar: [
-    { text: 'Docker 笔记', prefix: 'docker', collapsed: false, items: 'auto', },
-    { text: 'Kubernetes笔记', prefix: 'k8s/note', collapsed: false, items: 'auto', },
-    { text: 'YAML', prefix: 'k8s/yaml', collapsed: false, items: 'auto', },
-    { text: 'mysql 笔记', prefix: 'mysql', collapsed: false, items: 'auto', },
-  ],
-  */
 })
