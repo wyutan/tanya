3463f83a8a40e8a8b2ba1786896effb6127eb782
tanya <90786339+wyutan@users.noreply.github.com>
2026-02-21 23:36:33 +0800

    Update storage.ts

--- docs/.vuepress/notes/storage.ts
+++ docs/.vuepress/notes/storage.ts
@@ -3,9 +3,4 @@ import { defineNoteConfig } from 'vuepress-theme-plume'
 export default defineNoteConfig({
   dir: '/storage/',
   link: '/storage/',
-  /*
-  sidebar: [
-    { text: '存储笔记', prefix: 'note', collapsed: false, items: 'auto', },
-  ],
-  */
 })
