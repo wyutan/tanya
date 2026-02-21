f7ae34a0e44a659b6899eba6e9b9c07391dd88d1
tanya <90786339+wyutan@users.noreply.github.com>
2026-02-21 23:29:44 +0800

    Update linux.ts

--- docs/.vuepress/notes/linux.ts
+++ docs/.vuepress/notes/linux.ts
@@ -3,6 +3,7 @@ import { defineNoteConfig } from 'vuepress-theme-plume'
 export default defineNoteConfig({
   dir: 'os/linux',
   link: '/os/linux/',
+  /*
   sidebar: [
     { text: 'linux学习笔记', link: '/' },
     { text: 'linux笔记', prefix: 'note', collapsed: false, items: 'auto', },
@@ -10,4 +11,5 @@ export default defineNoteConfig({
     { text: '常用功能', prefix: '常用', collapsed: false, items: 'auto', },
     { text: '自动化运维', prefix: '自动化运维', collapsed: false, items: 'auto', },
   ]
+  */
 })
