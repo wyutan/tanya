e5b4da2e62d4c351671450717f54a6e868f879ae
tanya <90786339+wyutan@users.noreply.github.com>
2026-02-21 22:49:52 +0800

    Update linux.ts

--- docs/.vuepress/notes/linux.ts
+++ docs/.vuepress/notes/linux.ts
@@ -4,7 +4,7 @@ export default defineNoteConfig({
   dir: 'os/linux',
   link: '/os/linux/',
   sidebar: [
-    { text: 'linux学习笔记', link: '/os/linux/' },
+    { text: 'linux学习笔记', link: '/' },
     { text: 'linux笔记', prefix: 'note', collapsed: false, items: 'auto', },
     { text: 'linux命令', prefix: 'command', collapsed: false, items: 'auto', },
     { text: '常用功能', prefix: '常用', collapsed: false, items: 'auto', },
