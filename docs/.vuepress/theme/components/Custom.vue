f2dd9c9ee15b25ae09069fbbe3d6d6eb1fc71dfe
tanya <90786339+wyutan@users.noreply.github.com>
2026-02-21 18:03:09 +0800

    Update Custom.vue

--- docs/.vuepress/theme/components/Custom.vue
+++ docs/.vuepress/theme/components/Custom.vue
@@ -10,7 +10,7 @@
           <template #motto>
             <slot name="motto">
               <p class="about-me-card-title-normal">𝓂𝑜𝓉𝓉𝑜</p>
-              <p class="about-me-card-text-big about-me-card-text-color">是星辰，是雨雾<br>是闪电，是不羁的灵魂</p>
+              <p class="about-me-card-text-big about-me-card-text-color">是星辰，是雨雾<br>是闪电，是不羁的灵魂@[artPlayer](https://artplayer.org/assets/sample/video.mp4)</p>
             </slot>
           </template>
         </AboutMeText>
