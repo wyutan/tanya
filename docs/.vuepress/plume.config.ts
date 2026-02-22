e00f414b38b958a89b2d7b74b441241612b31617
tanya <90786339+wyutan@users.noreply.github.com>
2026-02-22 09:27:44 +0800

    Update plume.config.ts

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -1,6 +1,6 @@
 import { defineThemeConfig } from 'vuepress-theme-plume'
 import { navbar } from './navbar'
-import notes from './notes/index.ts'
+import notes from './index.ts'
 
 /**
  * @see https://theme-plume.vuejs.press/config/basic/
