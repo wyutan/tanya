ddfac1d14741ba2bb454c57a51d34afe9292059a
tanya <90786339+wyutan@users.noreply.github.com>
2026-02-22 09:24:32 +0800

    Update index.ts

--- docs/.vuepress/notes/index.ts
+++ docs/.vuepress/notes/index.ts
@@ -1,12 +1,4 @@
 import { defineNotesConfig } from 'vuepress-theme-plume'
-/*
-import network from './network.ts'
-import linux from './linux.ts'
-import cloud from './cloud.ts'
-import storage from './storage.ts'
-import 容器 from './容器.ts'
-import about from './about'
-*/
 export default defineNotesConfig({
   // 声明所有笔记的目录，(默认配置，通常您不需要声明它)
   dir: '',
@@ -19,13 +11,5 @@ export default defineNotesConfig({
     { dir: 'storage', link: '/storage', },
     { dir: '容器', link: '/容器', },
     { dir: 'about', link: '/about', },
-    /*
-    network,
-    linux,
-    cloud,
-    storage,
-    容器,
-    about,
-    */
   ]
 })
